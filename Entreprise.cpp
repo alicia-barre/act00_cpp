15593aa051fd0d0853415b40c2bacf3300a4750d
alicia-barre <alicia.barre2005@gmail.com>
2024-09-05 10:19:13 +0200

    modif entete

--- Entreprise.cpp
+++ Entreprise.cpp
@@ -2,9 +2,9 @@
  * \file   Entreprise.cpp
  * \brief  Definition de la classe Entreprise
  * 
- * \author Pierre
- * \version 0.1
- * \date   June 2023
+ * \author Alicia
+ * \version 1.0
+ * \date   June 2024
  * 
  * Programme Cpp permettant de creer des objets pour décrire les stages
  *********************************************************************/
